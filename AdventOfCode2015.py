ab4af861dbce8d65728a14cffacfa69ab590b64f
axel.lelong <axel.lelong@outlook.com>
2024-09-24 15:20:16 +0200

    added(day4) MD5 hash problem solved

--- AdventOfCode2015.py
+++ AdventOfCode2015.py
@@ -1,6 +1,7 @@
 from day01 import day01
 from day02 import day02
 from day03 import day03
+from day04 import day04
 
 day01.day1()
 print()
@@ -9,3 +10,7 @@ day02.day2()
 print()
 
 day03.day3()
+print()
+
+day04.day4()
+print()
---
+++ day04/day04.py
@@ -0,0 +1,19 @@
+import hashlib as hl
+
+def day4():
+
+    print("DAY 04")
+
+    file = open("day04/input.txt")
+    text = file.readline()
+    file.close()
+
+    hashtext = ""
+    index = 1
+    while(hashtext[0:6] != "000000"):
+        encoded = text + (str)(index)
+        hashtext = hl.md5(encoded.encode()).hexdigest()
+        index+=1
+        #print(hashtext)
+    
+    print(index-1)
