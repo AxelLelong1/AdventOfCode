351880d936c1643682f2965fab67c89be992aed7
axel.lelong <axel.lelong@outlook.com>
2024-12-11 11:13:22 +0100

    modified:   2024/AdventOfCode2024.py 	new file:   2024/day03/day03.py 	new file:   2024/day03/input.txt 	new file:   2024/day04/day04.py     new file:   2024/day04/input.txt     NEW DAY !! yeepee

--- 2024/AdventOfCode2024.py
+++ 2024/AdventOfCode2024.py
@@ -1,5 +1,7 @@
 from day01 import day01
 from day02 import day02
+from day03 import day03
 
 day01.day1()
 day02.day2()
+day03.day3()
---
+++ 2024/day04/day04.py
@@ -0,0 +1,5 @@
+def day04():
+    with open("day03/input.txt") as file:
+        data = file.readlines()
+
+    
